ee6f9d227f31d94f686bc363680d335735babec1
RomaChumak <romachumak355@gmail.com>
2023-10-13 21:44:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'Redux/Store';
-import { ThemeProvider } from 'styled-components';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
